7c98a5e64629e62a15e71cfa20c758e3915c2b2e
priyankar-laptop <priyankarkoley@gmail.com>
2023-05-18 23:17:51 +0530

    Activity Done

--- D_Activity Selection/main.c
+++ D_Activity Selection/main.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// A job has a start time, finish time and profit.
-struct Activitiy {
+// A job has a start time and finish time.
+typedef struct Activitiy {
 	int start, finish;
-};
+} Act;
 
-void printMaxActivities(Activitiy arr[], int n)
+void printMaxActivities(Act givenActs[], int n)
 {
-
-	cout << "Following activities are selected :\n";
-	int i = 0;
-	cout << "(" << arr[i].start << ", " << arr[i].finish<< ")";
-	for (int j = 1; j < n; j++) {
-		if (arr[j].start >= arr[i].finish) {
-			cout << ", (" << arr[j].start << ", "
-				<< arr[j].finish << ")";
+    int i, j;
+    for (i = 0; i < n - 1; i++){
+        for (j = 0; j < n - i - 1; ++j){
+            if (givenActs[j].finish > givenActs[j + 1].finish){
+                Act temp = givenActs[j];
+                givenActs[j] = givenActs[j + 1];
+                givenActs[j + 1] = temp;
+            }
+        }
+    }
+	printf("Following activities are selected :\n");
+	i=0;
+	printf("(%d, %d)", givenActs[i].start, givenActs[i].finish);
+	for (j = 1; j < n; j++)
+	{
+		if (givenActs[j].start >= givenActs[i].finish)
+        {
+			printf(", (%d, %d)",givenActs[j].start, givenActs[j].finish);
 			i = j;
 		}
 	}
@@ -23,9 +33,10 @@ void printMaxActivities(Activitiy arr[], int n)
 
 int main()
 {
-	Activitiy arr[] = { { 5, 9 }, { 1, 2 }, { 3, 4 },
-						{ 0, 6 }, { 5, 7 }, { 8, 9 } };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	printMaxActivities(arr, n);
+	Act givenActs[]={{1,3},{2,5},{3,4},{4,7},{7,10},{8,9},{9,11},{9,13},{11,12},{12,14}};
+	//1,3,4,6,7,9,10 th Acts
+	//1,3,4,8,9,11,12 --starting time
+	int n = sizeof(givenActs) / sizeof(givenActs[0]);
+	printMaxActivities(givenActs, n);
 	return 0;
 }
