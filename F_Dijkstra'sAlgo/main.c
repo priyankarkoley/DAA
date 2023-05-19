c1f082d0f8ca561705a4e952cbea17e40df00419
priyankar-laptop <priyankarkoley@gmail.com>
2023-05-19 10:24:47 +0530

    Main Function done

--- F_Dijkstra'sAlgo/main.c
+++ F_Dijkstra'sAlgo/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define INT_MAX 999999999
+
+void dijkstra(int n, int s, int cost[10][10], int dist[10])
+{
+
+}
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int i, j, n, s, cost[10][10], dist[10];
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            scanf("%d",&cost[i][j]);
+            if(cost[i][j]==0)cost[i][j]=INT_MAX;
+        }
+    }
+    printf("Enter the source vertex:");
+    scanf("%d", &s);
+    dijkstra(n,s,cost,dist);
+    printf("The shortest paths are: ");
+    for(i=1;i<=n;i++)
+        if(s!=i)printf("%d ---> %d = %d",s,i,dist[i]);
 }
