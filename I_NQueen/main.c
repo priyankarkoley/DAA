5e7187bc4afda7f83f990314e385a4eb74d86d17
priyankar-laptop <priyankarkoley@gmail.com>
2023-05-19 15:15:08 +0530

    NQUEEN done

--- I_NQueen/main.c
+++ I_NQueen/main.c
@@ -1,8 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int a[30], count = 0;
+
+int place(int pos)
+{
+    int i;
+    for (i = 1; i < pos;i++){
+        if((a[i]==a[pos]) || (abs(a[i] - a[pos])==abs(i-pos)))
+            return 0;
+    }
+    return 1;
+}
+
+void print_sol(int n)
+{
+    int i, j;
+    printf("\nSolution : %d\n", ++count);
+
+    for (i = 1; i <= n;i++){
+        for (j = 1; j <= n;j++){
+            if(a[i]==j)
+                printf("Q ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+
+void Queen(int n)
+{
+    int k = 1;
+    a[k] = 0;
+    while(k!=0){
+        a[k]++;
+        while((a[k]<=n)  && !place(k))
+            a[k]++;
+
+        if(a[k]<=n){
+            if(k==n)
+                print_sol(n);
+            else{
+                k++;
+                a[k] = 0;
+            }
+        }
+        else
+            k--;
+    }
+}
+
 
 int main()
 {
-    printf("Hello world!\n");
+    int n;
+    printf("\nEnter the no of queen: ");
+    scanf("%d", &n);
+
+    Queen(n);
+    printf("\nTotal Solution: %d", count);
+
     return 0;
 }
