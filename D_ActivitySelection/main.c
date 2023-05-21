f337be809de0458210bfa8661e3b0190f65fc83c
Priyankar Koley <priyankarkoley@gmail.com>
2023-05-21 22:56:12 +0530

    indentation fixed

--- D_ActivitySelection/main.c
+++ D_ActivitySelection/main.c
@@ -3,7 +3,7 @@
 
 // A job has a start time and finish time.
 typedef struct Activitiy {
-	int start, finish;
+    int start, finish;
 } Act;
 
 void printMaxActivities(Act givenActs[], int n)
@@ -19,24 +19,22 @@ void printMaxActivities(Act givenActs[], int n)
         }
     }
     printf("Following activities are selected :\n");
-	i=0;
-	printf("(%d, %d)", givenActs[i].start, givenActs[i].finish);
-	for (j = 1; j < n; j++)
-	{
-		if (givenActs[j].start >= givenActs[i].finish)
-        {
-			printf(", (%d, %d)",givenActs[j].start, givenActs[j].finish);
-			i = j;
-		}
-	}
+    i=0;
+    printf("(%d, %d)", givenActs[i].start, givenActs[i].finish);
+    for (j = 1; j < n; j++){
+        if (givenActs[j].start >= givenActs[i].finish){
+            printf(", (%d, %d)",givenActs[j].start, givenActs[j].finish);
+            i = j;
+        }
+    }
 }
 
 int main()
 {
-	Act givenActs[]={{1,3},{2,5},{3,4},{4,7},{7,10},{8,9},{9,11},{9,13},{11,12},{12,14}};
-	//1,3,4,6,7,9,10 th Acts
-	//1,3,4,8,9,11,12 --starting time
-	int n = sizeof(givenActs) / sizeof(givenActs[0]);
-	printMaxActivities(givenActs, n);
-	return 0;
+    Act givenActs[]={{1,3},{2,5},{3,4},{4,7},{7,10},{8,9},{9,11},{9,13},{11,12},{12,14}};
+    //1,3,4,6,7,9,10 th Acts
+    //1,3,4,8,9,11,12 --starting time
+    int n = sizeof(givenActs) / sizeof(givenActs[0]);
+    printMaxActivities(givenActs, n);
+    return 0;
 }
